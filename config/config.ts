771b5ac935f228d7c22e76f194522ae0a62a1df1
zzdoreen <zz1061831341@qq.com>
2024-04-30 15:35:56 +0800

    fix：build

--- config/config.ts
+++ config/config.ts
@@ -7,6 +7,7 @@ import proxy from './proxy'
 
 export default defineConfig({
     hash: true,
+    publicPath: './',
     antd: {},
     access: {},
     model: {},
@@ -15,7 +16,51 @@ export default defineConfig({
     layout: {
         title: '@umijs/max',
     },
+    devServer: { port: '6666' },
     routes,
     proxy: proxy['dev'],
     npmClient: 'yarn',
+    chainWebpack(config, { env }) {
+        if (env === 'production')
+            config.merge({
+                optimization: {
+                    runtimeChunk: false,
+                    splitChunks: {
+                        chunks: 'async',
+                        minSize: 30000,
+                        minChunks: 2,
+                        maxAsyncRequests: 5,
+                        maxInitialRequests: 3,
+                        automaticNameDelimiter: '.',
+                        cacheGroups: {
+                            vendor: {
+                                name: 'vendors',
+                                test({ resource }: any) {
+                                    return /[\\/]node_modules[\\/]@ant-design/.test(resource);
+                                },
+                                priority: 10,
+                            },
+                            bundle: {
+                                maxSize: 1500000,
+                                name: 'bundle',
+                                test({ resource }: any) {
+                                    return /[\\/]node_modules[\\/]/.test(resource);
+                                },
+                                priority: 5,
+                            },
+                            page: {
+                                minChunks: 1,
+                                maxSize: 1024000,
+                                name: 'page',
+                                priority: 1,
+                            },
+                        }
+                    }
+                }
+            })
+
+        config.module
+            .rule('mjs').test(/\.m?js$/)
+            .resolve.set('fullySpecified', false)
+    },
 });
