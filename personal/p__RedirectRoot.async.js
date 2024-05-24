7538fba80e20c9d9a382e767f57a38727c560eff
zzdoreen <zz1061831341@qq.com>
2024-05-24 17:00:33 +0800

    feat

---
+++ personal/p__RedirectRoot.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[401],{73058:function(l,f,n){n.r(f),n.d(f,{default:function(){return a}});var e=n(80854),r=n(67294),c=n(85893);function a(){var t=(0,r.useRef)(null);return(0,r.useEffect)(function(){var u;t.current&&((u=t.current)===null||u===void 0||u.click())},[]),(0,c.jsx)(e.NavLink,{to:"travel",ref:t})}}}]);
